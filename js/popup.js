eb184e33cecc1beaa1b0f07ab19ed55e15c84ba8
Mrunal Ahire <mrunal.ahire@gmail.com>
2018-12-25 11:17:35 +0530

    2nd commit

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,55 @@
-$(function(){
-    $('#name').keyup(function(){
-        $('#greet').text('Hello ' + $('#name').val())
-    })
-});
+//create and append close button to each task list item
+var jsTaskList=document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < jsTaskList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  jsTaskList[i].appendChild(span);
+}
+
+//hide item when close is clicked
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+//add tick before checked task
+var list = document.querySelector('taskList');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+//create new task when add is clicked
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("ip").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("taskList").appendChild(li);
+    }
+    document.getElementById("ip").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
